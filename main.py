53020bab9f04adc309ff71aaacd468f430c86a85
acceli <32413353+acceli@users.noreply.github.com>
2024-01-03 14:09:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from subprocess import Popen,PIPE
 from itchat.content import INCOME_MSG
 
 # 转发创米科技公众号视频到指定群聊
-send_chatrooms = ["蛋蛋的喵生"]
+send_chatrooms = ["撸猫群"]
 
 def download(url):
     r = requests.get(url)
-    video_url = re.findall(r'<source src="(.*?)" type="video/mp4">', r.text)[0]
+    video_url = re.findall(r'<source src="(.*?)"', r.text)[0]
     filename = time.strftime("%Y%m%d-%H%M%S.mp4")
-    Popen(f'ffmpeg -i {video_url} {filename} -y', shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()
+    Popen(f'ffmpeg -i "{video_url}" {filename} -y', shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()
     return filename
 
 def get_chatroom(nickname):
@@ -26,12 +26,15 @@ def get_chatroom(nickname):
 
 @itchat.msg_register(INCOME_MSG, isMpChat=True)
 def msg_reply(msg):
-    if msg["User"]["NickName"] == "创米科技":
+    if msg["User"]["NickName"] == "创米数联":
         filename = download(msg.Url)
         for nickname in send_chatrooms:
             toUserName = get_chatroom(nickname)
             itchat.send_video(fileDir=filename, toUserName=toUserName)
-        os.remove(filename)
+        try:
+            os.remove(filename)
+        except:
+            pass
 
 if __name__ == "__main__":
     itchat.auto_login(hotReload=True, enableCmdQR=2)
